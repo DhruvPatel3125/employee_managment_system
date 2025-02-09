f9fbcee65f7f7eed015430849ccf621a0736ec9e
dhruv patel <dhruvjpatel5@gmail.com>
2025-02-09 12:49:57 +0530

    feat: Refactor authentication context and implement AuthProvider, update Login component to handle login logic

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,20 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { setLocalStorage } from './utils/localStoreg'
+import { getLocalStorage, setLocalStorage } from './utils/localStoreg'
+import { useContext } from 'react'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
-  
-  useEffect(() => {
-    setLocalStorage()
-  },)
-  
+
+
+const [user, setUser] = useState(null)
+const handleLogin = (email,password)=>{
+  if(email == 'admin@me.com' && password == '123'){
+    setUser('admin')
+}else if(email == 'user@me.com' && password == '123'){
+  setUser('employee')
+}
+else{
+  alert("invalid credentials")
+}
+}
+const data = useContext(AuthContext)
+console.log(data)
   return (
     <>
-      <Login/>
-      {/* <EmployeeDashboard/> */}
-      {/* <AdminDashboard/> */}
+      {!user ?<Login handleLogin={handleLogin}/>:''}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard/>}
     </>
   )
 }
